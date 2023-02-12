2b5798703071c53884160fc889f39f4635b7d4e8
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-02-12 15:07:14 +0530

    Main Program.cs file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Program Using DATABASE.");
+        }
+    }
+ }
+ 
